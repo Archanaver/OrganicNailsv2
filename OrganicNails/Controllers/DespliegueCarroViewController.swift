d7f41ac0d825583931ce95d063c8ae905874ef4b
pedrodemiangodinezcruz <pppeperonni@hotmail.com>
2021-11-08 11:19:05 -0600

    Carrito
    Funciones carrito

--- OrganicNails/Controllers/DespliegueCarroViewController.swift
+++ OrganicNails/Controllers/DespliegueCarroViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by user189966 on 11/7/21.
 //
-/*
+
 import UIKit
 
 class DespliegueCarroViewController: UIViewController {
@@ -15,84 +15,76 @@ class DespliegueCarroViewController: UIViewController {
     @IBOutlet weak var CarritoTableView: UITableView!
     
     @IBOutlet weak var Total: UILabel!
-    var Total = 0
+
     var productos = [ProductoP]()
     var cursos = [CursoP]()
     
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        DespliegueCarritoTableView.delegate = self
-        DespliegueCarritoTableView.dataSource = self
-        
-        print("CURSOS", pedido?.cursos.count)
-        print("PRODUCTOS", pedido?.productos.count)
-        
-        for i in pedido!.productos {
-            productos.append(i)
-            Total = Total + productos?.precio ?? []
+        func viewDidLoad() {
+            super.viewDidLoad()
             
+            CarritoTableView.delegate = self
+            CarritoTableView.dataSource = self
+            
+            print("CURSOS", pedido?.cursos.count)
+            print("PRODUCTOS", pedido?.productos.count)
+            
+            for i in pedido!.productos {
+                productos.append(i)
+            }
+            
+            for i in pedido!.cursos {
+                cursos.append(i)
+            }
+     
+
+           
         }
         
-        for i in pedido!.cursos {
-            cursos.append(i)
-            Total = Total + producto?.precio ?? []
-        }
- 
 
-        // Do any additional setup after loading the view.
+       
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
-extension PedidosDetalleViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return cursos.count
+    extension DespliegueCarroViewController: UITableViewDelegate, UITableViewDataSource {
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 2
         }
-        else {
-            return productos.count
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            if section == 0 {
+                return cursos.count
+            }
+            else {
+                return productos.count
+            }
         }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "detallePedidoCell", for: indexPath)
-        if indexPath.section == 0 {
-            cell.textLabel?.text = cursos[indexPath.row].NombreCurso()
-            return cell
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "detallePedidoCell", for: indexPath)
+            if indexPath.section == 0 {
+                cell.textLabel?.text = cursos[indexPath.row].NombreCurso()
+                return cell
+            }
+            else {
+                cell.textLabel?.text = productos[indexPath.row].NombreProducto()
+                return cell
+            }
         }
-        else {
-            cell.textLabel?.text = productos[indexPath.row].NombreProducto()
-            return cell
+        
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            if section == 0 {
+                return "CURSOS"
+            }
+            else {
+                return "PRODUCTOS"
+            }
         }
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 0 {
-            return "CURSOS"
-        }
-        else {
-            return "PRODUCTOS"
-        }
-    }
     
     
 }
-*/
+
+
